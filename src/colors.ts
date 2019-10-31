c9af42bc75b560d8d0954d80cf929b9dea69a876
Steven Hillerman <31939792+shillerman@users.noreply.github.com>
2019-10-31 12:57:02 -0400

    Added colors.ts
    Added a ts file with the color pallete used in our examples.  Feel free to take the colors and put them in a better format if it would make things easier.

---
+++ src/colors.ts
@@ -0,0 +1,16 @@
+/* File: colors.ts
+ * Author: Steven Hillerman
+ * Date: 10/31/2019
+ * ----------------
+ * This file contains the color codes for the display.
+ */
+
+const DARK_BG = "0x333333"; // Main background color
+const LIGHTER_BG = "0x5F5F5F"; // Navigation bar background color
+const LIGHT_GREY = "0xACACAC"; // Grey used in graphs to stand out
+const TITLE_WHITE = "0xDADADA"; // Bright grey used in the title of each vis
+const TEAL = "0x00C89D"; // Teal used as an accent or highlight
+const SELECTION_GREEN = "0xA0C29E"; // Used to mark which view is selected in the navigation bar
+const TED_RED = "0x910400"; // Used in the circle packing charts as circle fill (LIGHT_GREY text and outlines)
+const BRIGHT_GREEN = "0x13B100"; // Good ratings in the ratings breakdown
+const BRIGHT_RED = "0xB10007"; // Bad ratings in the ratings breakdown
