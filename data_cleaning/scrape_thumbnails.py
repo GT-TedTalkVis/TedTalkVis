cd4cfc85bc669119fedc469ef070f5b73ea57d5f
s2lsoftener <s2@puyonexus.com>
2019-11-07 12:22:26 -0500

    YouTube data; All Page Data, but unparsed

--- data_cleaning/scrape_thumbnails.py
+++ data_cleaning/scrape_thumbnails.py
@@ -1,3 +1,4 @@
+#%%
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -21,15 +22,15 @@ def scrapePage(url: str):
         return np.NaN, np.NaN
     j = tag[start:end]
     j = json.loads(j)
-    speaker_data = j['__INITIAL_DATA__']['speakers']
+    # speaker_data = j['__INITIAL_DATA__']['speakers']
     thumbnail = j['__INITIAL_DATA__']['talks'][0]['hero']
-    return speaker_data, thumbnail
+    return j['__INITIAL_DATA__'], thumbnail
 
-
+#%%
 if __name__ == "__main__":
     # Load csv
     data_path = '../src/data/'
-    data_file = 'ted_main_new.csv'
+    data_file = 'ted_main_page_data.csv'
     data = pd.read_csv(data_path + data_file)
     
     # Argument parser
@@ -42,19 +43,20 @@ if __name__ == "__main__":
     start = args.start
     end = args.end if args.end <= len(data.index) else len(data.index)
     
-    speaker_data, thumbnail = scrapePage("https://www.ted.com/talks/tashka_and_laura_yawanawa_the_amazon_belongs_to_humanity_let_s_protect_it_together")
+    # page_data = scrapePage("https://www.ted.com/talks/tashka_and_laura_yawanawa_the_amazon_belongs_to_humanity_let_s_protect_it_together")
     count = 1
     with Bar('Processing', max = len(range(start, end)), fill = '*', suffix = '%(percent).1f%% - %(eta)ds') as bar: 
         for i in range(start, end):
             url = data.loc[i, "url"]
-            speaker_data, thumbnail = scrapePage(url)
-            data.loc[i, "speaker_data"] = str(speaker_data)
-            data.loc[i, "thumbnail"] = str(thumbnail)
+            page_data, thumbnail = scrapePage(url)
+            page_string = str(page_data)
+            data.loc[i, "page_data"] = page_string
+            # data.loc[i, "page_data_2"] = page_string[32767:(32767 * 2)]
             if count % 10 == 0:
                 data.to_csv(data_path + data_file)
             count += 1
             bar.next()
-            time.sleep(6 + random.randint(-1, 3))
+            time.sleep(4 + random.randint(-1, 2))
     data.to_csv(data_path + data_file)
 
     # page = requests.get("https://www.ted.com/talks/chris_anderson_of_wired_on_tech_s_long_tail")
